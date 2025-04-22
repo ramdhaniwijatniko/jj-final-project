5038c8ff5f4ad500965e36133a768308c690dc2c
dhaniwijat_ <ramdhani.wijatniko@jamkrida-jakarta.co.id>
2025-04-22 15:15:46 +0700

    Initial commit: update web and API automation framework

--- src/test/java/web/stepdefinitions/Hooks.java
+++ src/test/java/web/stepdefinitions/Hooks.java
@@ -3,10 +3,12 @@ package web.stepdefinitions;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
     private static WebDriver driver;
@@ -14,23 +16,27 @@ public class Hooks {
     @Before
     public void setUp() {
         if (driver == null) {
-            driver = new ChromeDriver();
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage");
+            driver = new ChromeDriver(options);
             driver.manage().window().maximize();
         }
     }
 
     @After
     public void tearDown(Scenario scenario) {
-        if (scenario.isFailed()) {
+        if (scenario.isFailed() && driver != null) {
             byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
             scenario.attach(screenshot, "image/png", "screenshot");
         }
-    
+
         if (driver != null) {
             driver.quit();
             driver = null;
         }
     }
+
     public static WebDriver getDriver() {
         return driver;
     }
